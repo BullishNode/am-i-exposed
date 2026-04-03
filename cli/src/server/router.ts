7f93463237832a462f430cd1d263ca0fe3d52f9c
Francis Pouliot <francis@satoshiportal.com>
2026-04-03 12:39:39 -0600

    fix: CORS headers include PUT and Authorization in response

--- cli/src/server/router.ts
+++ cli/src/server/router.ts
@@ -94,8 +94,8 @@ export function sendJson(res: ServerResponse, status: number, data: unknown): vo
   res.writeHead(status, {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
   });
   res.end(body);
 }
